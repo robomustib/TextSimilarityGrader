043d94ea937da5ae79f9895ac28d6f9e6e8c1730
Mustafa <robots@mustafabilgin.de>
2026-02-02 11:04:06 +0100

    Update create_test_data.py

--- tests/create_test_data.py
+++ tests/create_test_data.py
@@ -1,3 +1,9 @@
+"""
+TextSimilarityGrader (https://github.com/robomustib/TextSimilarityGrader/)
+Copyright (c) 2026 Mustafa Bilgin
+Licensed under Creative Commons Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)
+"""
+
 import json
 import os
 from pathlib import Path
